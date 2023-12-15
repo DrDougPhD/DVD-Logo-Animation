b0ac8352f8a7a69759aaa417623c5461e168a4e1
Doug McGeehan <doug@kmnr.org>
2023-12-15 16:16:53 -0700

    stitch together stuff

--- stitch.sh
+++ stitch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -framerate 60 -f image2 -i ./output/%02d.png -c:v libvpx-vp9 -pix_fmt yuva420p ./output/dvd.webm
+ffmpeg -framerate ${1} -f image2 -i "${2}" -c:v libvpx-vp9 -pix_fmt yuva420p ./output/dvd.webm
